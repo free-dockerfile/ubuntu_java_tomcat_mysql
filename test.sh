9d5be89258d56bc532a901153a0b39348949393c
jack <jack@1225.hk>
2016-12-28 17:59:14 +0800

    update volum

--- test.sh
+++ test.sh
@@ -6,7 +6,7 @@ docker rm -f test01
 docker rmi test:v1
 docker build --no-cache=true -t=test:v1 .
 sleep 5
-docker create --name test01 -i -t -h test01 -p 9001:4200 -p 9002:22 -p 9003:3306 -p 9004:8080 test:v1 
+docker create --name test01 -i -t -h test01 -v /src/app_data/solution/example:/src/lib/tomcat/default/webapps/ROOT -p 9001:4200 -p 9002:22 -p 9003:3306 -p 9004:8080 test:v1 
 docker start test01 
 sleep 10
 #docker logs test01
