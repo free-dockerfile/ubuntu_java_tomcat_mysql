77f331d7f07eaa8b2d1ad5e3d0dbedd3500346f4
jack <jack@1225.hk>
2016-12-28 17:31:14 +0800

    update test.sh

--- test.sh
+++ test.sh
@@ -5,6 +5,7 @@
 docker rm -f test01 
 docker rmi test:v1
 docker build --no-cache=true -t=test:v1 .
+sleep 5
 docker create --name test01 -i -t -h test01 -p 9001:4200 -p 9002:22 -p 9003:3306 -p 9004:8080 test:v1 
 docker start test01 
 sleep 10
