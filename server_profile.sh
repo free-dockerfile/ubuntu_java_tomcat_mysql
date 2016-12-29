d490e17942c39529905b6a8a6bb42833d7d00a2b
jack <jack@1225.hk>
2016-12-29 15:13:20 +0800

    update export path

--- server_profile.sh
+++ server_profile.sh
@@ -3,4 +3,4 @@
 export JAVA_HOME=/usr/local/src/lib/java/default 
 export CATALINA_HOME=/usr/local/src/lib/tomcat/default 
 export ANT_HOME=/usr/local/src/lib/ant/default 
-export PATH=/usr/local/src/lib/java/default/bin:/usr/local/src/lib/ant/default/bin:/usr/local/src/lib/tomcat/default/bin:$PATH
+export PATH=$JAVA_HOME/bin:$ANT_HOME/bin:$CATALINA_HOME/bin:$PATH
